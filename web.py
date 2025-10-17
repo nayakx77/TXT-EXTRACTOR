fefb482020428084649ab993944adb26be7a60d3
nayakx77 <n8455297@gmail.com>
2025-10-17 18:33:44 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
-from flask import Flask
+wafrom flask import Flask
 
 web_app = Flask(__name__)
 
 @web_app.route('/')
 def home():
-    return "Bot is running by SPIDY UNIVERSE"
+    return "Bot is running by NAYAK UNIVERSE"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
   
+ok 0 lo 
